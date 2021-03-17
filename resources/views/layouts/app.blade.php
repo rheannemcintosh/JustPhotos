4fce2e9acca3ada1b2c1188818cb2d5bba6a0395
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-17 23:30:56 +0000

    Display the profile image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,9 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <img src="{{ Storage::url(auth()->user()->profile_picture) }}" width="50">
                                     {{ Auth::user()->name }}
+
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
