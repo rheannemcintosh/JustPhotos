4857f4bc4b116bc60269549445e66ea13c0b88b9
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-19 23:30:23 +0000

    Create a vue album component to display the create form

---
+++ resources/js/components/AlbumComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <form enctype="multipart/form-date">
+            <div class="form-group">
+                <label>Name of Album</label>
+                <input type="text" name="name" class="form-control" maxLength="15">
+            </div>
+            <div class="form-group">
+                <label>Album Description</label>
+                <textarea name="description" class="form-control" maxLength="200"></textarea>
+            </div>
+            <div class="form-group">
+                <label>Album Category</label>
+                <select class="form-control">
+                    <option></option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label>Album Cover</label>
+                <input type="file" name="image" class="form-control">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-secondary">Create Album</button>
+            </div>
+        </form>
+    </div>
+</template>
