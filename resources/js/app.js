10581e0e92e002937b4da27f1c20eb167163a84d
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-25 23:24:32 +0000

    Update javascript

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,8 @@ window.Vue = require('vue').default;
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('album', require('./components/AlbumComponent.vue').default);
+Vue.component('index', require('./components/IndexComponent.vue').default);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
