ff425df8d0774e52a56931cbc9102cfb0dc88d33
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-26 22:11:24 +0000

    Update the display albums page

--- resources/views/album/index.blade.php
+++ resources/views/album/index.blade.php
@@ -2,12 +2,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('All Albums') }}</div>
-                <index></index>
-            </div>
+    <ol class="breadcrumb">
+        <h3>Your album</h3>
+        <span style="margin-left: 700px;">
+            <a href="{{ route('album.create') }}">
+                <button class="btn btn-primary">
+                    Create New Album
+                </button>
+            </a>
+        </span>
+    </ol>
+    <div clas="row">
+        <div class="col-md-12">
+            <index></index>
         </div>
     </div>
 </div>
