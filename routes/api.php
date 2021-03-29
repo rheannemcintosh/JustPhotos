fe41e18ceee9334a54f3b43c1ba0c0750b22ed35
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-29 23:10:09 +0100

    Fix error with the get one album api route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AlbumController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/categories', [CategoryController::class, 'getCategories']);
+Route::get('albums/{id}',[AlbumController::class, 'getOneAlbum']);
