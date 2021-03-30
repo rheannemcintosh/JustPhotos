1a6088ebdce21ce4cb06df9eb91401125d8d3a66
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-31 00:23:00 +0100

    Add a sweet alert for when the album is updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
