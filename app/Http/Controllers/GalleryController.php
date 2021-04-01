21248fefee06b9219b2cde55fafa790ef7c0a3b4
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-04-01 23:42:35 +0100

    Create a Gallery Controller

---
+++ app/Http/Controllers/GalleryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Image;
+
+class GalleryController extends Controller
+{
+    public function create(){
+        return view('image.create');
+    }
+
+    public function upload(Request $request){
+        foreach ($request->file('files') as $file) {
+            $name = $file->hashName();
+            $file->move(public_path().'/images/',$name);
+
+            $file = new Image;
+            $file->album_id = $request->album_id;
+            $file->image = $name;
+            $file->save();
+        }
+        return response()->json(['success' => 'Your images have been uploaded']);
+    }
+}
