9ee0f4c30533e1dba45788a16655ed0b9e7c36fe
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-04-03 13:33:25 +0100

    Create an upload button on the IndexComponent

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -8,6 +8,9 @@
                     <th scope="col">Name</th>
                     <th scope="col">Description</th>
                     <th scope="col">Category</th>
+                    <th scope="col">Upload</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
                 </tr>
             </thead>
             <tbody>
@@ -21,6 +24,11 @@
                     <td>{{album.name}}</td>
                     <td>{{album.description}}</td>
                     <td>{{album.category.name}}</td>
+                    <td>
+                        <a :href="'upload/images/'+album.id">
+                            <button class="btn btn-success">Upload</button>
+                        </a>
+                    </td>
                     <td>
                         <button @click.prevent="edit(album.id)" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                             Edit
