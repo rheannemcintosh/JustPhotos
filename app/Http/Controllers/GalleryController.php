18c8d49eba54c98a2456efa4a36411a6f92dc79f
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-04-03 13:32:33 +0100

    Update the create view for album images

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Image;
+use App\Models\Album;
 
 class GalleryController extends Controller
 {
-    public function create(){
-        return view('image.create');
+    public function create($album_id){
+        $albumBelongsToUser = Album::where('user_id', auth()->user()->id)->where('id', $album_id)->exists();
+
+        if ($albumBelongsToUser) {
+            session()->put('id', $album_id);
+            return view('image.create', compact('album_id'));
+        } else {
+            return redirect()->back();
+        }
+        
     }
 
     public function upload(Request $request){
