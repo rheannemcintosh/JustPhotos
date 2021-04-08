fdd551563926a63cc9e8e0583998aded7577758b
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-04-08 23:43:22 +0100

    Create two follow methods on the User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function following ()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'following_id');
+    }
+
+    public function amIFollowing ($userId)
+    {
+        return \DB::table('followers')->where('follower_id', auth()->user()->id)->where('following_id', $userId)->exists();
+    }
 }
