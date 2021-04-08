263091a063b79a35e5ae0d32e9cbb593dc8ecc6b
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-04-08 23:41:38 +0100

    Create a follow method in the Follower Controller

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class FollowerController extends Controller
 {
-    //
+    public function follow (Request $request)
+    {
+        $followerId = User::find(auth()->user()->id);
+        $followingId = User::find($request->userId);
+        $followerId->following()->toggle($followingId);
+
+        return redirect()->back();
+    }
 }
