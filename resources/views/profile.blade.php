eaff18977a020b53af873e70d8c2f193f58fd9a9
rheannemcintosh <rheannemcintosh@gmail.com>
2021-06-08 23:47:27 +0100

    Update profile view to display the background banner

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,21 +10,20 @@
                 <div class="card-body">
 
                     <ul class="nav nav-tabs" role="tablist">
-                        <li class="nav-item">
-                          <a class="nav-link active" href="#profile" role="tab" data-toggle="tab">Profile</a>
-                        </li>
-                        <li class="nav-item">
+                        <li class="nav-item active">
                           <a class="nav-link" href="#buzz" role="tab" data-toggle="tab">Following</a>
                         </li>
                         <li class="nav-item">
-                          <a class="nav-link" href="#references" role="tab" data-toggle="tab">Profile picture</a>
+                          <a class="nav-link" href="#profilepic" role="tab" data-toggle="tab">Profile Picture</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#bgpic" role="tab" data-toggle="tab">Background Picture</a>
                         </li>
                       </ul>
                       
                       <!-- Tab panes -->
                       <div class="tab-content">
-                        <div role="tabpanel" class="tab-pane fade in active" id="profile">Manage your profile</div>
-                        <div role="tabpanel" class="tab-pane fade" id="buzz">
+                        <div role="tabpanel" class="tab-pane active" id="buzz">
                             @foreach($followings as $follow)
                                 <p>
                                     {{ $follow->userfollow->name }}
@@ -39,8 +38,11 @@
                                 </p>
                             @endforeach
                         </div>
-                        <div role="tabpanel" class="tab-pane fade" id="references">
+                        <div role="tabpanel" class="tab-pane fade" id="profilepic">
                             <avatar userid="{{auth()->user()->id}}"></avatar>
+                        </div>
+                        <div role="tabpanel" class="tab-pane fade" id="bgpic">
+                            <background userid="{{auth()->user()->id}}"></background>
                         </div>
                       </div>
                     
